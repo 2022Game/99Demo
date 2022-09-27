306289f31b63a020f0bb518a6e40a9e2212c985f
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki-net.ne.jp>
2022-09-27 10:32:45 +0900

    2

--- 3DLv1_2022_vs2019/GameProgramming/src/CApplication.cpp
+++ 3DLv1_2022_vs2019/GameProgramming/src/CApplication.cpp
@@ -1,6 +1,7 @@
 #include "CApplication.h"
 #include "CRectangle.h"
 #include "glut.h"
+#include "CVector.h"
 
 //クラスのstatic変数
 CTexture CApplication::mTexture;
@@ -26,22 +27,49 @@ void CApplication::Start()
 
 void CApplication::Update()
 {
+	//頂点1､頂点2､頂点3,法線データの作成
+	CVector v0, v1, v2, n;
+	//法線を上向きで設定する
+	n.Set(0.0f, 1.0f, 0.0f);
+	//頂点1の座標を設定する
+	v0.Set(0.0f, 0.0f, 0.5f);
+	//頂点2の座標を設定する
+	v1.Set(1.0f, 0.0f, 0.0f);
+	//頂点3の座標を設定する
+	v2.Set(0.0f, 0.0f, -0.5f);
+
 	gluLookAt(1.0f, 2.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 
 	glBegin(GL_TRIANGLES);
 
-	glNormal3f(0.0f, 1.0f, 0.0f);
+	//法線（面の向き）の設定
+	glNormal3f(n.X(), n.Y(), n.Z());
+	//頂点座標の設定
+	glVertex3f(v0.X(), v0.Y(), v0.Z());
+	glVertex3f(v1.X(), v1.Y(), v1.Z());
+	glVertex3f(v2.X(), v2.Y(), v2.Z());
 
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, -0.5f);
+	//法線と頂点の設定
+	n.Set(0.0f, 0.0f, 1.0f);
+	v0.Set(0.5f, 0.0f, 0.0f);
+	v1.Set(0.0f, 1.0f, 0.0f);
+	v2.Set(-0.5f, 0.0f, 0.0f);
+	//三角形2の描画
+	glNormal3f(n.X(), n.Y(), n.Z());
+	glVertex3f(v0.X(), v0.Y(), v0.Z());
+	glVertex3f(v1.X(), v1.Y(), v1.Z());
+	glVertex3f(v2.X(), v2.Y(), v2.Z());
 
-
-	glNormal3f(0.0f, 0.0f, 1.0f);
-
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(-0.5f, 0.0f, 0.0f);
+	//法線と頂点の設定
+	n.Set(1.0f, 0.0f, 0.0f);
+	v0.Set(0.0f, 0.5f, 0.0f);
+	v1.Set(0.0f, 0.0f, 1.0f);
+	v2.Set(0.0f, -0.5f, 0.0f);
+	//三角形2の描画
+	glNormal3f(n.X(), n.Y(), n.Z());
+	glVertex3f(v0.X(), v0.Y(), v0.Z());
+	glVertex3f(v1.X(), v1.Y(), v1.Z());
+	glVertex3f(v2.X(), v2.Y(), v2.Z());
 
 	glEnd();
 }
