257cf1465c3192bdb6b98b689d8529f9635b9153
Takahashi-Shinobu-Fukuyama <sinobu-takahashi@anabuki-net.ne.jp>
2024-05-21 15:11:53 +0900

    18T

--- 2DLv1_00_x64vs2022/GameProgramming64/CApplication.cpp
+++ 2DLv1_00_x64vs2022/GameProgramming64/CApplication.cpp
@@ -13,36 +13,78 @@ void CApplication::Start()
 	mFont.Load("FontWhite.png", 1, 64);
 	mBullet.Set(400.0f, -98.0f, 3.0f, 10.0f);
 	mMiss.Set(400.0f, 630.0f, 400.0f, 10.0f);
+	mState = EState::EPLAY;
 }
 
 void CApplication::Update()
 {
-	if (mInput.Key(VK_SPACE))
+	switch (mState)
 	{
-		mBullet.Set(mPlayer.X(),
-			mPlayer.Y() + mPlayer.H() + mBullet.H(),
-			3.0f, 10.0f);
-		mBullet.Move();
-	}
+	case EState::EPLAY:
+		if (mInput.Key(VK_SPACE))
+		{
+			mBullet.Set(mPlayer.X(),
+				mPlayer.Y() + mPlayer.H() + mBullet.H(),
+				3.0f, 10.0f);
+			mBullet.Move();
+		}
 
-	mPlayer.Update();
-	mEnemy.Update();
-	mBullet.Update();
-	mPlayer.Render();
-	mEnemy.Render();
-	mBullet.Render();
+		mPlayer.Update();
+		mEnemy.Update();
+		mBullet.Update();
+		mPlayer.Render();
+		mEnemy.Render();
+		mBullet.Render();
 
-	mEnemy.Collision(&mBullet);
-	if (mBullet.Collision(&mEnemy))
-	{
-		mFont.Draw(370.0f, 300.0f, 15.0f, 30.0f, 'H');
-		mFont.Draw(400.0f, 300.0f, 15.0f, 30.0f, 'I');
-		mFont.Draw(430.0f, 300.0f, 15.0f, 30.0f, 'T');
+		mEnemy.Collision(&mBullet);
+		if (mBullet.Collision(&mEnemy))
+		{
+			mFont.Draw(370.0f, 300.0f, 15.0f, 30.0f, 'H');
+			mFont.Draw(400.0f, 300.0f, 15.0f, 30.0f, 'I');
+			mFont.Draw(430.0f, 300.0f, 15.0f, 30.0f, 'T');
+			mFont.Draw(370.0f, 240.0f, 15.0f, 30.0f, "PUSH");
+			mFont.Draw(370.0f, 180.0f, 15.0f, 30.0f, "ENTER");
+			mState = EState::ECLEAR;
+		}
+		if (mBullet.Collision(&mMiss))
+		{
+			mFont.Draw(370.0f, 300.0f, 15.0f, 30.0f, "MISS");
+			mState = EState::EOVER;
+		}
+		break;
+	case EState::EOVER:
+		mPlayer.Render();
+		mEnemy.Render();
+		mBullet.Render();
+		mFont.Draw(370.0f, 300.0f, 15.0f, 30.0f, "MISS");
 		mFont.Draw(370.0f, 240.0f, 15.0f, 30.0f, "PUSH");
 		mFont.Draw(370.0f, 180.0f, 15.0f, 30.0f, "ENTER");
-	}
-	if (mBullet.Collision(&mMiss))
-	{
-		mFont.Draw(370.0f, 300.0f, 15.0f, 30.0f, "MISS");
+		if (mInput.Key(VK_RETURN))
+		{
+			mState = EState::ESTART;
+		}
+		break;
+	case EState::ECLEAR:
+		mPlayer.Render();
+		mEnemy.Render();
+		mBullet.Render();
+		mFont.Draw(370.0f, 300.0f, 15.0f, 30.0f, "HIT");
+		mFont.Draw(370.0f, 240.0f, 15.0f, 30.0f, "PUSH");
+		mFont.Draw(370.0f, 180.0f, 15.0f, 30.0f, "ENTER");
+		if (mInput.Key(VK_RETURN))
+		{
+			mState = EState::ESTART;
+		}
+		break;
+	default:
+		mPlayer.Set(400.0f, 44.0f, 26.0f, 44.0f);
+		mPlayer.Texture(&mTexture, 740, 876, 1236,
+			1016);
+		mEnemy.Set(26.0f, 574.0f, 44.0f, 26.0f);
+		mEnemy.Texture(&mTexture, 1604, 1808, 680,
+			472);
+		mBullet.Set(400.0f, -98.0f, 3.0f, 10.0f);
+		mState = EState::EPLAY;
+		break;
 	}
 }
--- 2DLv1_00_x64vs2022/GameProgramming64/CApplication.h
+++ 2DLv1_00_x64vs2022/GameProgramming64/CApplication.h
@@ -11,11 +11,22 @@
 class CApplication
 {
 public:
+public:
+	enum class EState
+	{
+		ESTART,	//ゲーム開始
+		EPLAY,	//ゲーム中
+		ECLEAR,	//ゲームクリア
+		EOVER,	//ゲームオーバー
+	};
+
 	//最初に一度だけ実行するプログラム
 	void Start();
 	//繰り返し実行するプログラム
 	void Update();
 private:
+	EState mState;
+
 	CMiss mMiss;
 	CInput mInput;
 	CFont mFont;
